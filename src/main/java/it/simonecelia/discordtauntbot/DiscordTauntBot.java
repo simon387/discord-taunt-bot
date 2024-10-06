5990fdb7009f5ede4e260902e0c80d848f6d55c3
Simone Celia <simone.celia@eng.it>
2024-10-06 03:51:31 +0200

    refa

--- src/main/java/it/simonecelia/discordtauntbot/DiscordTauntBot.java
+++ src/main/java/it/simonecelia/discordtauntbot/DiscordTauntBot.java
@@ -1,12 +1,6 @@
 package it.simonecelia.discordtauntbot;
 
-import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import it.simonecelia.discordtauntbot.audio.AudioPlayer;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -17,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Objects;
 import java.util.Properties;
 
 
@@ -25,16 +18,12 @@ public class DiscordTauntBot extends ListenerAdapter {
 
 	private static final Logger log = LoggerFactory.getLogger ( DiscordTauntBot.class );
 
-	private final AudioPlayerManager playerManager;
-
-	private final TrackScheduler trackScheduler;
+	private final AudioPlayer audioPlayer;
 
 	private final String ASSETS_DIR;
 
 	public DiscordTauntBot () {
-		this.playerManager = new DefaultAudioPlayerManager ();
-		AudioSourceManagers.registerLocalSource ( playerManager );
-		this.trackScheduler = new TrackScheduler ( playerManager.createPlayer () );
+		audioPlayer = new AudioPlayer ();
 		//
 		var currentPath = new File ( "" ).getAbsolutePath ();
 		log.info ( "App working dir is: {}", currentPath );
@@ -86,13 +75,13 @@ public class DiscordTauntBot extends ListenerAdapter {
 			var filePath = content.substring ( 6 ); // 6 è la lunghezza di "/play "
 			filePath = filePath.trim ();
 			var file = ASSETS_DIR + filePath + ".mp3"; // Aggiungi l'estensione ".mp3"
-			playAudio ( event, file );
+			audioPlayer.playAudio ( event, file );
 			return;
 		}
 
 		if ( content.equals ( "/stop" ) ) {
 			log.info ( "Stopping audio playback" );
-			stopAudio ( event );
+			audioPlayer.stopAudio ( event );
 			return;
 		}
 
@@ -100,56 +89,6 @@ public class DiscordTauntBot extends ListenerAdapter {
 			log.info ( "Listing all commands " );
 			event.getChannel ().sendMessage ( "/ping - pong\n /play <audio file>\n /stop\n /list this list" ).queue ();
 		}
-	}
-
-	private void playAudio ( MessageReceivedEvent event, String audioFile ) {
-		var voiceChannel = Objects.requireNonNull (
-						Objects.requireNonNull ( Objects.requireNonNull ( event.getMember () ).getVoiceState () ).getChannel () ).asVoiceChannel ();
-
-		var audioManager = event.getGuild ().getAudioManager ();
-		audioManager.setSendingHandler ( new AudioPlayerSendHandler ( trackScheduler.getPlayer () ) );
-		audioManager.openAudioConnection ( voiceChannel );
-
-		playerManager.loadItem ( audioFile, new AudioLoadResultHandler () {
-
-			@Override
-			public void trackLoaded ( AudioTrack track ) {
-				trackScheduler.queue ( track );
-				log.info ( "Riproduzione di: {}", audioFile );
-				event.getChannel ().sendMessage ( "Riproduzione di: " + audioFile ).queue ();
-			}
-
-			@Override
-			public void playlistLoaded ( AudioPlaylist playlist ) {
-				// Non gestito per semplicità
-			}
-
-			@Override
-			public void noMatches () {
-				log.error ( "File audio non trovato: {}", audioFile );
-				event.getChannel ().sendMessage ( "File audio non trovato: " + audioFile ).queue ();
-			}
-
-			@Override
-			public void loadFailed ( FriendlyException exception ) {
-				log.error ( "Errore nel caricamento del file audio: {}", exception.getMessage () );
-				event.getChannel ().sendMessage ( "Errore nel caricamento del file audio: " + exception.getMessage () ).queue ();
-			}
-		} );
-	}
-
-	private void stopAudio ( MessageReceivedEvent event ) {
-		// Ferma la riproduzione del brano corrente
-		trackScheduler.getPlayer ().stopTrack ();
-
-		// Ottieni il canale vocale e chiudi la connessione audio
-		var audioManager = event.getGuild ().getAudioManager ();
-		if ( audioManager.isConnected () ) {
-			audioManager.closeAudioConnection ();
-		}
-
-		// Invia un messaggio di conferma
-		event.getChannel ().sendMessage ( "Audio fermato." ).queue ();
 	}
 
 }
