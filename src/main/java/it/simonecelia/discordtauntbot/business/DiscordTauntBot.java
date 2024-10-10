57049177fdbd3a1f2e9ca0525874cffad51d07c2
Simone Celia <simone.celia@simonecelia.it>
2024-10-11 01:51:45 +0200

    added first scheduler

--- src/main/java/it/simonecelia/discordtauntbot/business/DiscordTauntBot.java
+++ src/main/java/it/simonecelia/discordtauntbot/business/DiscordTauntBot.java
@@ -1,7 +1,9 @@
 package it.simonecelia.discordtauntbot.business;
 
 import it.simonecelia.discordtauntbot.audio.AudioPlayer;
+import it.simonecelia.discordtauntbot.scheduler.KoTHScheduler;
 import it.simonecelia.discordtauntbot.text.TextSender;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.slf4j.Logger;
@@ -18,19 +20,18 @@ public class DiscordTauntBot extends ListenerAdapter {
 
 	private final TextSender textSender;
 
-	private final String adminID;
+	private final DTBInput input;
 
-	private boolean verbose;
+	private KoTHScheduler koTHScheduler;
 
-	public DiscordTauntBot ( String adminID, boolean verbose ) {
+	public DiscordTauntBot ( DTBInput input ) {
+		this.input = input;
 		textSender = new TextSender ();
 		var currentPath = new File ( "" ).getAbsolutePath ();
 		log.info ( "App working dir is: {}", currentPath );
 		audioPlayer = new AudioPlayer ( currentPath );
-		this.adminID = adminID;
-		log.info ( "Admin ID is: {}", this.adminID );
-		this.verbose = verbose;
-		log.info ( "Verbose is: {}", this.verbose );
+		log.info ( "Admin ID is: {}", input.getAdminID () );
+		log.info ( "Verbose is: {}", input.isVerbose () );
 	}
 
 	@Override
@@ -43,21 +44,22 @@ public class DiscordTauntBot extends ListenerAdapter {
 		log.info ( "Got message from: {}, with Content: {}", event.getAuthor (), content );
 
 		switch ( content ) {
-		case String c when c.startsWith ( "/p " ) || c.startsWith ( "/play " ) -> audioPlayer.playAudio ( event, content, this.verbose );
-		case "/stop" -> audioPlayer.stopAudio ( event, this.verbose );
+		case String c when c.startsWith ( "/p " ) || c.startsWith ( "/play " ) -> audioPlayer.playAudio ( event, content, this.input.isVerbose () );
+		case "/stop" -> audioPlayer.stopAudio ( event, this.input.isVerbose () );
 		case "/tauntlist" -> textSender.sendTauntList ( event );
 		case "/links" -> textSender.sendLinks ( event );
 		case "/list" -> textSender.sendCmdList ( event );
 		case "/version" -> textSender.sendVersion ( event );
 		case "/verbose" -> {
 			if ( isFromAdmin ( event ) ) {
-				this.verbose = !this.verbose;
-				log.info ( "Verbose enabled: {}", this.verbose );
-				event.getChannel ().sendMessage ( "Verbose enabled:" + this.verbose ).queue ();
+				this.input.setVerbose ( !this.input.isVerbose () );
+				log.info ( "Verbose enabled: {}", this.input.isVerbose () );
+				event.getChannel ().sendMessage ( "Verbose enabled:" + this.input.isVerbose () ).queue ();
 			}
 		}
 		case "/kill" -> {
 			if ( isFromAdmin ( event ) ) {
+				koTHScheduler.shutdown ();
 				System.exit ( 0 );
 			}
 		}
@@ -68,7 +70,10 @@ public class DiscordTauntBot extends ListenerAdapter {
 
 	private boolean isFromAdmin ( MessageReceivedEvent event ) {
 		log.info ( "Checking if Author {} is admin", event.getAuthor () );
-		return event.getAuthor ().getId ().equals ( adminID );
+		return event.getAuthor ().getId ().equals ( this.input.getAdminID () );
 	}
 
+	public void setSchedulerAndJDA ( JDA jda ) {
+		koTHScheduler = new KoTHScheduler ( textSender, this.input, jda );
+	}
 }
---
+++ src/main/java/it/simonecelia/discordtauntbot/scheduler/KoTHScheduler.java
@@ -0,0 +1,58 @@
+package it.simonecelia.discordtauntbot.scheduler;
+
+import it.simonecelia.discordtauntbot.business.DTBInput;
+import it.simonecelia.discordtauntbot.text.TextSender;
+import net.dv8tion.jda.api.JDA;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.Executors.newScheduledThreadPool;
+
+
+public class KoTHScheduler {
+
+	private static final Logger log = LoggerFactory.getLogger ( KoTHScheduler.class );
+
+	private final ScheduledExecutorService scheduler;
+
+	private final TextSender textSender;
+
+	private final DTBInput input;
+
+	private final JDA jda;
+
+	public KoTHScheduler ( TextSender textSender, DTBInput input, JDA jda ) {
+		this.textSender = textSender;
+		this.input = input;
+		this.jda = jda;
+		log.info ( "Current time is: {}", LocalTime.now () );
+		this.scheduler = newScheduledThreadPool ( 1 );
+		scheduleTaskAt ( LocalTime.of ( 1, 0 ) );  // Schedule the task to trigger at a specific time
+		scheduleTaskAt ( LocalTime.of ( 1, 31 ) );
+		scheduleTaskAt ( LocalTime.of ( 17, 0 ) ); // E.g., Trigger the task at 17:00 (5:00 PM)
+	}
+
+	public void shutdown () {
+		scheduler.shutdown ();
+	}
+
+	private void scheduleTaskAt ( LocalTime targetTime ) {  // Method to schedule a task at a specific time
+		var now = LocalTime.now ();
+		var initialDelay = Duration.between ( now, targetTime ).toMillis ();
+		if ( initialDelay < 0 ) {
+			initialDelay += Duration.ofDays ( 1 ).toMillis ();  // If the target time is before the current time, schedule for the next day
+		}
+		scheduler.scheduleAtFixedRate ( this::triggeredTask, initialDelay, TimeUnit.DAYS.toMillis ( 1 ), TimeUnit.MILLISECONDS );
+		log.info ( "Task scheduled to trigger at {}", targetTime );
+	}
+
+	private void triggeredTask () {  // The task to be triggered
+		log.info ( "Triggered task at scheduled time: {}", LocalTime.now () );
+		textSender.sendKotHAlert ( input.getGuildID (), input.getChannelID (), jda );
+	}
+}
