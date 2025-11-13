d8cd1373a09785b5529863fd8d012547aa1d610e
Simone Celia <simone.celia@simonecelia.it>
2025-11-13 12:28:04 +0100

    1.5.14

--- src/main/java/it/simonecelia/discordtauntbot/DiscordTauntBotApp.java
+++ src/main/java/it/simonecelia/discordtauntbot/DiscordTauntBotApp.java
@@ -7,8 +7,13 @@ import it.simonecelia.discordtauntbot.service.business.DiscordTauntBot;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 @Startup
@@ -22,7 +27,7 @@ public final class DiscordTauntBotApp {
 	DiscordTauntBot discordTauntBot;
 
 	@PostConstruct
-	public void onStartup () throws InterruptedException {
+	public void onStartup () throws InterruptedException, IOException {
 		Log.info ( "Starting Discord Taunt Bot" );
 
 		if ( appConfig.getDiscordBotToken () == null || appConfig.getDiscordBotToken ().isEmpty () ) {
@@ -37,7 +42,25 @@ public final class DiscordTauntBotApp {
 		var jda = jdaBuilder.build ();
 
 		jda.awaitReady (); // Blocking until JDA is ready
+		setNickName ( jda );
 		Log.info ( "Bot connected" );
+
 		appConfig.setJda ( jda );
+	}
+
+	private void setNickName ( JDA jda ) throws IOException {
+		var versionInfo = "No version found!";
+		try ( var in = getClass ().getClassLoader ().getResourceAsStream ( "changelog.txt" ) ) {
+			if ( in != null ) {
+				try ( var reader = new BufferedReader ( new InputStreamReader ( in ) ) ) {
+					versionInfo = reader.readLine ().trim ();
+				}
+			}
+		}
+		final String finalVersionInfo = " " + versionInfo.replaceFirst ( "Version ", "v" );
+		jda.getGuilds ().forEach ( guild -> guild.modifyNickname ( guild.getSelfMember (), "TauntBot" + finalVersionInfo ).queue (
+						success -> Log.infof ( "Nickname changed in guild %s with %s", guild.getName (), finalVersionInfo ),
+						error -> Log.warnf ( "Could not change nickname in guild %s: %s", guild.getName (), error.getMessage () )
+		) );
 	}
 }
