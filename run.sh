467f0fbe423178a85a6aff3261efaa84a428456a
Simone Celia <simone.celia@simonecelia.it>
2025-12-03 23:03:29 +0100

    1.6.20

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-git pull;mvn clean install package;java -Xmx2g -jar target/quarkus-app/quarkus-run.jar
+git pull;mvn clean install package;java -jar target/quarkus-app/quarkus-run.jar
+#git pull;mvn clean install package;java -Xmx2g -jar target/quarkus-app/quarkus-run.jar
 
